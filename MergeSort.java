26283b47080586a220184fffc0a9cf45b6383833
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-09-26 14:01:29 -0700

    Fixed a bug where I was passing in the wrong array size

--- MergeSort.java
+++ MergeSort.java
@@ -1,42 +1,54 @@
 import java.util.Arrays;
 
 public class MergeSort implements SortingAlgorithm {
-	
+	/*
+	public static void main(String[] args) {
+		int[] test = new int[] {1, 5, 2, 7, 4};
+		//System.out.println("Array before sort: " + Arrays.toString(test));
+		sort(test);
+		//System.out.println("Array after sort: " + Arrays.toString(test));
+
+	}
+	*/
 	public void sort(int[] a) {
 		mergeSort(a, 0, a.length - 1);
 	}
 	
-	void mergeSort(int[] a, int left, int right) {
+	static void mergeSort(int[] a, int left, int right) {
 		if (right <= left) {
 			return;
 		}
 		int midPoint = (left + right)/2;
+		//System.out.println("My broken left Array is: " + Arrays.toString(Arrays.copyOfRange(a, left, midPoint + 1)));
+		//System.out.println("My broken right Array is: " + Arrays.toString(Arrays.copyOfRange(a, midPoint + 1, right + 1)));
 		mergeSort(a, left, midPoint);
 		mergeSort(a, midPoint + 1, right);
-		merge(a, left, right);
+		merge(a, midPoint, left, right);
 	}
 	
-	void merge(int[] a, int left, int right) {
-		int mid = (left + right)/ 2;
-		merge(Arrays.copyOfRange(a, left, mid + 1), Arrays.copyOfRange(a, mid + 1, right), a);
+	static void merge(int[] a, int mid, int left, int right) {
+		//System.out.println("Left Array: " + Arrays.toString(Arrays.copyOfRange(a, left, mid + 1)));
+		//System.out.println("Right Array: " + Arrays.toString(Arrays.copyOfRange(a, mid + 1, right + 1)));
+		//System.out.println("Whole Array: " + Arrays.toString(a));
+		merge(left, Arrays.copyOfRange(a, left, mid + 1), Arrays.copyOfRange(a, mid + 1, right + 1), a);
 
 	}
 	
-	void merge(int[] leftArr, int[] rightArr, int[] a) {
-		int left = 0, right = 0, target = 0;
-		while (left < leftArr.length && right < rightArr.length) {
-			if (leftArr[left] <= rightArr[right]) {
-				a[target++] = leftArr[left++];
+	static void merge(int left, int[] leftArr, int[] rightArr, int[] a) {
+		int leftCount = 0, rightCount = 0, target = left;
+		while (leftCount < leftArr.length && rightCount < rightArr.length) {
+			if (leftArr[leftCount] <= rightArr[rightCount]) {
+				a[target++] = leftArr[leftCount++];
 			} else {
-				a[target++] = rightArr[right++];
+				a[target++] = rightArr[rightCount++];
 			}
 
 		}
-		while (left < leftArr.length) {
-			a[target++] = leftArr[left++];
+		while (leftCount < leftArr.length) {
+			a[target++] = leftArr[leftCount++];
 		}
-		while (right < rightArr.length) {
-			a[target++] = rightArr[right++];
+		while (rightCount < rightArr.length) {
+			a[target++] = rightArr[rightCount++];
 		}
 
 	}
