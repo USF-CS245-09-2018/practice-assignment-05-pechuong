a3680c4055f4722a78235e71c30d5607735ed1b0
Peter Chuong <peter@Peters-MacBook-Pro.local>
2018-09-30 23:46:46 -0700

    Fixed bug and got everything working

--- QuickSort.java
+++ QuickSort.java
@@ -1,51 +1,48 @@
 import java.util.Arrays;
 
-public class QuickSort /*implements SortingAlgorithm*/ {
+public class QuickSort implements SortingAlgorithm {
 
+	/*
 	public static void main(String[] args) {
 		int[] test = new int[] {3, 5, 1, 2, 4};
+		System.out.println("Beginning Array: " + Arrays.toString(test));
 		sort(test);
-		System.out.println(Arrays.toString(test));
+		System.out.println("Ending Array: " + Arrays.toString(test));
 
 	}
+	*/
 
-	public static void sort(int[] a) {
+	public void sort(int[] a) {
 		quickSort(a, 0, a.length - 1);
 	}
 	
-	public static void quickSort(int[] a, int left, int right) {
+	public void quickSort(int[] a, int left, int right) {
 		if (left < right) {
 			int pivot = partition(a, left, right);
-			System.out.println("left = " + left);
-			System.out.println("right = " + right);
-			System.out.println("pivot = " + pivot);
-
 			quickSort(a, left, pivot - 1);
-			System.out.println("Hello I'm starting to do quickSort on the right side");
 			quickSort(a, pivot + 1, right);
 		}
 	}
 
-	public static int partition(int[] a, int left, int right) {
-			int pivot = left;
-			int i = left + 1;
-			int k = right;
-			System.out.println(Arrays.toString(a));
-			while (i < k) {
-				while (i < right && a[i] < a[pivot]) {
-					i++;
-				}
-				while (k > i && a[k] > a[pivot]) {
-					k--;
-				}
-				if (i < k) {
-					swap(a, i, k);
-				}
-				System.out.println(Arrays.toString(a));
+	public int partition(int[] a, int left, int right) {
+		int pivot = left;
+		int i = left + 1;
+		int k = right;
+
+		while (i < k) {
+			while (i < right && a[i] <= a[pivot]) {
+				i++;
 			}
-			swap(a, pivot, k);
-			System.out.println(Arrays.toString(a));
-			return k;
+			while (k >= i && a[k] > a[pivot]) {
+				k--;
+			}
+			if (i < k) {
+				swap(a, i, k);
+			}
+		}
+		swap(a, pivot, k);
+		return k;
+		
 	}
 
 	static void swap(int[] a, int index1, int index2) {
